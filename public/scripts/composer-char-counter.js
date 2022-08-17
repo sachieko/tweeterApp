55c43f0127fc21939233be42291697a47003bed5
sachieko <arashikabane@gmail.com>
2022-08-16 21:23:27 -0600

    Implemented character counter with styles

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,10 +1,11 @@
 // eslint-disable-next-line no-undef
 $(function() {
-  const newTweet = document.getElementById('tweet-text');
+  const textLimit = function() {
+    const $counter = $(this).parent().next().children('.counter');
+    const length = 140 - this.value.length;
+    length >= 0 ? $counter.css('color', 'rgb(84, 81, 73)') : $counter.css('color', 'red');
+    $counter.val(length);
+  };
   
-
-  newTweet.addEventListener('input', function(event) {
-    const count = document.getElementsByClassName('counter')[0];
-    count.innerHTML = 140 - this.value.length;
-  });
+  $("#tweet-text").on('input', textLimit);
 });
