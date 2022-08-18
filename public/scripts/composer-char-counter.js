e5dfa4bf0e5d3c8e824cbabe3dfb4ffa65f5ad46
sachieko <arashikabane@gmail.com>
2022-08-18 15:31:40 -0600

    Lint comments for eslint

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,13 +1,11 @@
-// eslint-disable-next-line no-undef
+/* eslint-disable no-undef */
 $(function() {
   const textLimit = function() {
-    // eslint-disable-next-line no-undef
     const $counter = $(this).parent().next().children('.counter');
     const length = 140 - this.value.length;
     length >= 0 ? $counter.css('color', 'rgb(84, 81, 73)') : $counter.css('color', 'red');
     $counter.val(length);
   };
   
-  // eslint-disable-next-line no-undef
   $("#tweet-text").on('input', textLimit);
 });
