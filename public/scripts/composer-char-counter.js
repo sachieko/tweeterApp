1a26e9536e378fb75bc26d6f3251348caa60f017
sachieko <arashikabane@gmail.com>
2022-08-19 02:38:55 -0600

    Comments

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,9 +1,12 @@
 /* eslint-disable no-undef */
+//
+//This function uses jquery to put a listener on the input, and gets the length of the text in the input textarea, and
+//if that length - char limit is negative the counter element associated with that input turns red.
 $(function() {
   const textLimit = function() {
     const $counter = $(this).parent().next().children('.counter');
     const length = 140 - this.value.length;
-    length >= 0 ? $counter.css('color', 'rgb(84, 81, 73)') : $counter.css('color', 'red');
+    length >= 0 ? $counter.css('color', '#545149') : $counter.css('color', 'red');
     $counter.val(length);
   };
   
